bf6888ce3a18df579911964d34ce8baa704d6d99
SAUMYAGOYAL25 <saumyagoyal2510@gmail.com>
2026-06-24 17:48:10 +0530

    Create SlidingWindowMaximum.py

---
+++ Week_3/DAY_5/SlidingWindowMaximum.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        if not nums or k == 0:
+            return []
+
+        dq = deque()
+        result = []
+
+        for i in xrange(len(nums)):
+            while dq and dq[0] <= i - k:
+                dq.popleft()
+
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+
+            dq.append(i)
+
+            if i >= k - 1:
+                result.append(nums[dq[0]])
+
+        return result
