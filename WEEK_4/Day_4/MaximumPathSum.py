67858db7fc746871cb59d59a25de5489dcc4b968
SAUMYAGOYAL25 <saumyagoyal2510@gmail.com>
2026-06-26 15:37:42 +0530

    Create MaximumPathSum.py

---
+++ WEEK_4/Day_4/MaximumPathSum.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.max_sum = float('-inf')
+        self.dfs(root)
+        return self.max_sum
+
+    def dfs(self, node):
+        if not node:
+            return 0
+
+        left = max(0, self.dfs(node.left))
+        right = max(0, self.dfs(node.right))
+
+        self.max_sum = max(self.max_sum, node.val + left + right)
+
+        return node.val + max(left, right)
